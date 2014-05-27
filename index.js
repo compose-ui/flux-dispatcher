c8deb6acd5a4de0d15bc232a562a98f6033c486f
taterbase <shankga@gmail.com>
2014-05-27 16:34:07 -0600

    Initial

---
+++ index.js
@@ -0,0 +1,16 @@
+var _callbacks = []
+
+module.exports = Dispatcher
+
+function Dispatcher(){}
+
+Dispatcher.prototype.register = function(cb) {
+  _callbacks.push(cb)
+  return true;
+}
+
+Dispatcher.prototype.dispatch = function(payload) {
+  _callbacks.forEach(function(cb) {
+    cb.apply(cb, payload)
+  })
+}
